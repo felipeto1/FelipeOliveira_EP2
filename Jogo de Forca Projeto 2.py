9c60b94538bcb01891fb4d8ed60e757f9ab20c69
felipeto1 <felipeto1@al.insper.edu.br>
2015-03-24 21:39:34 -0300

    Colocando as letras nos lugares

--- Jogo de Forca Projeto 2.py
+++ Jogo de Forca Projeto 2.py
@@ -12,41 +12,141 @@ window = turtle.Screen()
 window.bgcolor("gray")
 window.title("Jogo da Forca")
 
-q=int(input('Aperte zero para começar - '))
-if q==0:
-    turtle.setpos(320.00,0.00) #criando a forca
-    turtle.setpos(-275.00,0.00)
-    turtle.setpos(-275.00,200.00)
-    turtle.setpos(-160.00,200.00)
-    turtle.setpos(-160.00,180.00)
-    turtle.penup()
-    turtle.setpos(-130.00,15.00)
-    turtle.pendown()
-    turtle.hideturtle()
+turtle.setpos(320.00,0.00) #criando a forca
+turtle.setpos(-275.00,0.00)
+turtle.setpos(-275.00,200.00)
+turtle.setpos(-160.00,200.00)
+turtle.setpos(-160.00,180.00)
+turtle.penup()
+turtle.setpos(-145.00,15.00)
+turtle.pendown()
+turtle.hideturtle()
 sup=0
 while sup<=11:   
     erros=0
-
     escolha=choice(palavra)
     palavra.remove(escolha)
 
     tamanho=len(escolha)
     h=0
-    while (h <=tamanho or erros<=6):
-        for u in escolha:
-            if u!= ' ':
-                turtle.forward(20)
-            else:
-                turtle.penup()
-                turtle.forward(20)
-                turtle.pendown()
+    for u in escolha:
+        if u!= ' ':
+            turtle.forward(20)
+        else:
             turtle.penup()
-            turtle.forward(5)
+            turtle.forward(20)
             turtle.pendown()
+        turtle.penup()
+        turtle.forward(5)
+        turtle.pendown()
+    while (h<=tamanho or erros!=7):
         letra=window.textinput('letra','escolha uma letra')
-    
         if letra in escolha:
-            turtle.write(letra)
+            if letra==escolha[0]:
+                turtle.penup()
+                turtle.setpos(-135.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[1]:
+                turtle.penup()
+                turtle.setpos(-115.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[2]:
+                turtle.penup()
+                turtle.setpos(-95.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[3]:
+                turtle.penup()
+                turtle.setpos(-75.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[4]:
+                turtle.penup()
+                turtle.setpos(-55.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[5]:
+                turtle.penup()
+                turtle.setpos(-35.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[6]:
+                turtle.penup()
+                turtle.setpos(-15.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[7]:
+                turtle.penup()
+                turtle.setpos(5.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[8]:
+                turtle.penup()
+                turtle.setpos(15.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[9]:
+                turtle.penup()
+                turtle.setpos(35.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[10]:
+                turtle.penup()
+                turtle.setpos(55.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[11]:
+                turtle.penup()
+                turtle.setpos(75.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[12]:
+                turtle.penup()
+                turtle.setpos(95.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[13]:
+                turtle.penup()
+                turtle.setpos(115.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[14]:
+                turtle.penup()
+                turtle.setpos(135.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[15]:
+                turtle.penup()
+                turtle.setpos(155.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[16]:
+                turtle.penup()
+                turtle.setpos(175.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[17]:
+                turtle.penup()
+                turtle.setpos(195.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[18]:
+                turtle.penup()
+                turtle.setpos(215.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[19]:
+                turtle.penup()
+                turtle.setpos(235.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
+            if letra==escolha[20]:
+                turtle.penup()
+                turtle.setpos(255.00,20.00)
+                turtle.pendown()
+                turtle.write(letra)
             h+=1
         else:
             erros+=1
@@ -103,6 +203,7 @@ while sup<=11:
             turtle.pendown()
             turtle.setpos(-160.00,80.00)
             turtle.setpos(-170.00,40.00)
+            
             
         turtle.penup()
         turtle.setpos(-145.00,15.00)
