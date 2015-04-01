4fbc25ca1675194e1ce5c8d90e6612fcb4cbf5eb
felipeto1 <felipeto1@al.insper.edu.br>
2015-03-31 22:00:31 -0300

    erros finais

--- Jogo de Forca Projeto 2.py
+++ Jogo de Forca Projeto 2.py
@@ -37,6 +37,7 @@ while repetiçoes<11: #definindo rodadas
         escolha=escolha.strip()
         tamanho=len(escolha)
         o=escolha.count(" ")
+        lista=[]
         if escolha!=" ":
             for u in escolha: #criando as linhas debaixo das letras
                 if u!= ' ':
@@ -50,12 +51,23 @@ while repetiçoes<11: #definindo rodadas
                 turtle.pendown()
             while (h<tamanho-o and erros<6): #criando o loop principal
                 letra=window.textinput('letra','escolha uma letra') #janela de escrever a letra
-                lista=[]
-                lista.append(letra)
-                if len(letra)==1:
+                if len(letra)==1 and letra not in lista:
                     n=escolha.count(letra)
                     j=0
                     x=-1
+                    aaa=escolha.find('ã') #condições para acentos e maiúsculas
+                    aaaa=escolha.find('â')
+                    eee=escolha.find('é')
+                    eeee=escolha.find('ê')
+                    iii=escolha.find('í')
+                    ooo=escolha.find('ó')
+                    oooo=escolha.find('ô')
+                    sss=escolha.find('S')
+                    bbb=escolha.find('B')
+                    ccc=escolha.find('C')
+                    aa=escolha.find('A')
+                    ee=escolha.find('E')
+                    ppp=escolha.find('P')
                     while j<n:
                         x=escolha.index(letra,x+1) #colocando as letras nos lugares
                         j+=1
@@ -65,9 +77,99 @@ while repetiçoes<11: #definindo rodadas
                             turtle.pendown()
                             turtle.write(letra)
                             h+=1
-                
-                    
-                    if letra not in escolha:
+                    if letra == "a" and escolha[aaa] == "ã" : #aplicando acentos
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*aaa),10.00)
+                        turtle.pendown()
+                        turtle.write('ã')
+                        h+=1
+            
+                    if letra == "a" and escolha[aaaa]=="â" :
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*aaaa),10.00)
+                        turtle.pendown()
+                        turtle.write('â')
+                        h+=1
+                       
+                    if letra == "e" and escolha[eee] == 'é':
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*eee),10.00)
+                        turtle.pendown()
+                        turtle.write('é')
+                        h+=1
+                       
+                    if letra == "e" and escolha[eeee] == "ê":
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*eeee),10.00)
+                        turtle.pendown()
+                        turtle.write('ê')
+                        h+=1
+                      
+                    if letra == 'o' and escolha[ooo] == 'ó':
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*ooo),10.00)
+                        turtle.pendown()
+                        turtle.write('ó')
+                        h+=1
+                     
+                    if letra == "o" and escolha[oooo] == "ô":
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*oooo),10.00)
+                        turtle.pendown()
+                        turtle.write('ô')
+                        h+=1
+                        erros-=1
+                    if letra == 'i' and escolha[iii] == 'í':
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*iii),10.00)
+                        turtle.pendown()
+                        turtle.write('í') 
+                        h+=1
+                        erros-=1
+                    if letra == "s" and escolha[sss] == "S": # aplicando maiúsculas
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*sss),10.00)
+                        turtle.pendown()
+                        turtle.write('S')
+                        h+=1
+                        erros-=1
+                    if letra == 'e' and escolha[ee] == 'E':
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*ee),10.00)
+                        turtle.pendown()
+                        turtle.write('E')
+                        h+=1
+                        erros-=1
+                    if letra == "b" and escolha[bbb] == "B":
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*bbb),10.00)
+                        turtle.pendown()
+                        turtle.write('B')
+                        h+=1
+                        erros-=1
+                    if letra == 'c' and escolha[ccc] == 'C':
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*ccc),10.00)
+                        turtle.pendown()
+                        turtle.write('C') 
+                        h+=1
+                        erros-=1
+                    if letra == "a" and escolha[aa] == "A":
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*aa),10.00)
+                        turtle.pendown()
+                        turtle.write('A')
+                        h+=1
+                        erros-=1
+                    if letra == 'p' and escolha[ppp] == 'P':
+                        turtle.penup()
+                        turtle.setpos(-190.00+(25*ppp),10.00)
+                        turtle.pendown()
+                        turtle.write('P')
+                        h+=1
+                        erros-=1
+                   
+                    elif letra not in escolha:
                         erros+=1
                         if erros==1: #criando a cabeça
                             turtle.st()
@@ -139,11 +241,18 @@ while repetiçoes<11: #definindo rodadas
                         turtle.pendown()
                         turtle.write(scoref)
                         v+=1
-                else:
+                elif len(letra)!=1:
                     turtle.penup()
                     turtle.setpos(00.00,200.00)
                     turtle.pendown()
                     turtle.write('Você só pode escolher uma letra por vez')
+                else:
+                    turtle.penup()
+                    turtle.setpos(65.00,40.00)
+                    turtle.pendown()
+                    turtle.write('Você já escolheu essa letra')
+                lista.append(letra)
+                
             turtle.penup()
             turtle.setpos(-100.00,-100.00)
             turtle.pendown()
